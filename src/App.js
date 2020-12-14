1d636288290cd6b7ab7e71bb445b044fcb8aa7a6
Akshay Laxmipathi <52963912+Akshay-Laxmipathi@users.noreply.github.com>
2020-12-14 16:33:54 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,66 @@
+import React, { useState } from "react";
+import "./styles.css";
+
+var emojiDictionary = {
+  "😀": "Grinning Face",
+  "😄": "Grinning Face with Smiling Eyes",
+  "😁": "Beaming Face with Smiling Eyes",
+  "😅": "Grinning Face with Sweat",
+  "🤣": "Rolling on the Floor Laughing",
+  "😂": "Face with Tears of Joy",
+  "🙂": "Slightly Smiling Face",
+  "😉": "Winking Face",
+  "😍": "Smiling Face with Heart Eyes",
+  "🤩": "Star Struck",
+  "😘": "Face Blowing a Kiss",
+  "😋": "Face Savoring Food",
+  "😛": "Face with Stuck Out Tongue",
+  "😜": "Winking Face with Tongue",
+  "🤔": "Thinking Face",
+  "😑": "Expressionless Face"
+};
+
+var emojisWeKnow = Object.keys(emojiDictionary);
+
+export default function App() {
+  const [meaning, setMeaning] = useState("");
+
+  function emojiInputHandler(event) {
+    var userInput = event.target.value;
+
+    var meaning = emojiDictionary[userInput];
+
+    if (meaning === undefined) {
+      meaning = "we don't have this emoji in our database";
+    }
+    setMeaning(meaning);
+  }
+
+  function emojiClickHandler(emoji) {
+    var meaning = emojiDictionary[emoji];
+    setMeaning(meaning);
+  }
+
+  return (
+    <div className="App">
+      <h1>Emoji Dictionary</h1>
+      <input onChange={emojiInputHandler}></input>
+
+      <h2>{meaning}</h2>
+
+      <h3>emojis we know</h3>
+      {emojisWeKnow.map(function (emoji) {
+        return (
+          <span
+            onClick={() => emojiClickHandler(emoji)}
+            style={{ fontSize: "2rem", padding: "0.5rem", cursor: "pointer" }}
+            key={emoji}
+          >
+            {" "}
+            {emoji}{" "}
+          </span>
+        );
+      })}
+    </div>
+  );
+}
